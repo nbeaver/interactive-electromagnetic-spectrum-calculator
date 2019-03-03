767ee589c405055c0c438db6d56b2984ec50cf01
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2019-03-03 14:21:07 -0600

    Make this a separate function.

--- spectrum-slider.js
+++ spectrum-slider.js
@@ -4,48 +4,53 @@ function formatNumber(number) {
   return scientificNotation;
 }
 
+function updateValue(id, string) {
+  var element = document.getElementById(id);
+  element.value = string;
+}
+
 function calculate() {
   var slider_value = parseFloat(document.getElementById('slider').value);
 
   var wavelength = Math.pow(10.0, slider_value);
-  document.getElementById('wavelength').value = formatNumber(wavelength);
+  updateValue('wavelength', formatNumber(wavelength));
 
   const m_to_nm = 1e+9;
   var wavelength_nm = wavelength * m_to_nm;
-  document.getElementById('wavelength_nm').value = wavelength_nm.toPrecision(4);
+  updateValue('wavelength_nm', wavelength_nm.toPrecision(4));
 
   const m_to_angstrom = 1e+10;
   var wavelength_angstrom = wavelength * m_to_angstrom;
-  document.getElementById('wavelength_angstrom').value = wavelength_angstrom.toPrecision(4);
+  updateValue('wavelength_angstrom', wavelength_angstrom.toPrecision(4));
 
   const c = 299792458; // m/s
 
   var frequency = c / wavelength;
-  document.getElementById('frequency').value = formatNumber(frequency);
+  updateValue('frequency', formatNumber(frequency));
 
   const h = 6.62607e-34; // kg m^2 / s
   var energy_J = h * frequency;
-  document.getElementById('energy_J').value = formatNumber(energy_J);
+  updateValue('energy_J', formatNumber(energy_J));
 
   const J_to_eV = 6.2415091e+18;
   var energy_eV = energy_J * J_to_eV;
-  document.getElementById('energy_eV').value = energy_eV.toPrecision(4);
+  updateValue('energy_eV', energy_eV.toPrecision(4));
 
   var momentum = h / wavelength;
-  document.getElementById('momentum').value = formatNumber(momentum);
+  updateValue('momentum', formatNumber(momentum));
 
   const kg_m_s_to_eV_c = 1.8711574e+27;
 
   // Same as eV, but still good to know.
   var momentum_eV_c = momentum * kg_m_s_to_eV_c;
-  document.getElementById('momentum_eV_c').value = momentum_eV_c.toPrecision(4);
+  updateValue('momentum_eV_c', momentum_eV_c.toPrecision(4));
 
   var wavenumber = 2 * Math.PI / wavelength;
-  document.getElementById('wavenumber').value = formatNumber(wavenumber);
+  updateValue('wavenumber', formatNumber(wavenumber));
 
   const inv_m_to_inv_cm = 0.01;
   var wavenumber_inv_cm = wavenumber * inv_m_to_inv_cm;
-  document.getElementById('wavenumber_inv_cm').value = formatNumber(wavenumber_inv_cm);
+  updateValue('wavenumber_inv_cm', formatNumber(wavenumber_inv_cm));
 
 }
 
