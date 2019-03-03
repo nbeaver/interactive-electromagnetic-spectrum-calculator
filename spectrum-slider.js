40d4e460e6adc10236e02a21bdb5872af7a14584
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2019-03-03 14:02:14 -0600

    Add wavenumber.

--- spectrum-slider.js
+++ spectrum-slider.js
@@ -31,6 +31,22 @@ function calculate() {
   var energy_eV = energy_J * J_to_eV;
   document.getElementById("energy_eV").value = energy_eV.toPrecision(4);
 
+  var momentum = h / wavelength;
+  document.getElementById("momentum").value = formatNumber(momentum);
+
+  const kg_m_s_to_eV_c = 1.8711574e+27;
+
+  // Same as eV, but still good to know.
+  var momentum_eV_c = momentum * kg_m_s_to_eV_c;
+  document.getElementById("momentum_eV_c").value = momentum_eV_c.toPrecision(4);
+
+  var wavenumber = 2 * Math.PI / wavelength;
+  document.getElementById("wavenumber").value = formatNumber(wavenumber);
+
+  const inv_m_to_inv_cm = 0.01;
+  var wavenumber_inv_cm = wavenumber * inv_m_to_inv_cm;
+  document.getElementById("wavenumber_inv_cm").value = formatNumber(wavenumber_inv_cm);
+
 }
 
 window.onload = function() {
