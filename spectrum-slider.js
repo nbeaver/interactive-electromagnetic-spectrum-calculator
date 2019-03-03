b36ff4196b0063e21b53baee772db92bb14f0e8b
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2019-03-02 22:30:44 -0600

    More fields.

--- spectrum-slider.js
+++ spectrum-slider.js
@@ -10,10 +10,26 @@ function calculate() {
   var wavelength = Math.pow(10.0, slider_value);
   document.getElementById("wavelength").value = formatNumber(wavelength);
 
+  const m_to_nm = 1e+9;
+  var wavelength_nm = wavelength * m_to_nm;
+  document.getElementById("wavelength_nm").value = wavelength_nm.toPrecision(4);
+
+  const m_to_angstrom = 1e+10;
+  var wavelength_angstrom = wavelength * m_to_angstrom;
+  document.getElementById("wavelength_angstrom").value = wavelength_angstrom.toPrecision(4);
+
   const c = 299792458; // m/s
 
   var frequency = c / wavelength;
   document.getElementById("frequency").value = formatNumber(frequency);
+
+  const h = 6.62607e-34 // kg m^2 / s
+  var energy_J = h * frequency;
+  document.getElementById("energy_J").value = formatNumber(energy_J);
+
+  const J_to_eV = 6.2415091e+18
+  var energy_eV = energy_J * J_to_eV;
+  document.getElementById("energy_eV").value = energy_eV.toPrecision(4);
 
 }
 
